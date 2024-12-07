9d7e066c04f675cd11d54220dded1b42c64fd729
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-07 12:21:01 -0500

    Redundant if
    Signed-off-by: kevinmelkowski <kevin.melkowski@gmail.com>

--- src/main/scala/Solutions/Day7.scala
+++ src/main/scala/Solutions/Day7.scala
@@ -5,7 +5,7 @@ import utils.Problem
 object Day7 extends Problem[List[(Long, List[Long])]](2024, 7) {
   private def checkCalculation(values: List[Long], total: Long, result: Long, concat: Boolean = false): Boolean =
     values match {
-      case Nil => if (result == total) true else false
+      case Nil => result == total
       case x :: xs => checkCalculation(xs, total, result + x, concat) || checkCalculation(xs, total, result * x, concat)
         || (if (concat) checkCalculation(xs, total, (result.toString + x.toString).toLong, concat) else false)
     }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,4 @@
 import Solutions.*
-import cats.effect.IO
-import cats.effect.unsafe.implicits.global
-import utils.Timer
-
-import scala.annotation.tailrec
-import scala.io.Source
 
 @main
 def main(): Unit = Day7.run()
