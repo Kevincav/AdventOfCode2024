784c59c102a77b282d53232a5a776d3c4b5f555b
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-07 11:15:33 -0500

    Recursive call to map
    Signed-off-by: kevinmelkowski <kevin.melkowski@gmail.com>

--- src/main/scala/Solutions/Day7.scala
+++ src/main/scala/Solutions/Day7.scala
@@ -2,7 +2,7 @@ package Solutions
 
 import utils.Problem
 
-object Day7 extends Problem[Map[BigInt, List[BigInt]]](2024, 7) {
+object Day7 extends Problem[List[(BigInt, List[BigInt])]](2024, 7) {
   private def checkCalculation(values: List[BigInt], total: BigInt, result: BigInt, concat: Boolean = false): Boolean =
     values match {
       case Nil => if (result == total) true else false
@@ -10,14 +10,12 @@ object Day7 extends Problem[Map[BigInt, List[BigInt]]](2024, 7) {
         || (if (concat) checkCalculation(xs, total, BigInt(result.toString + x.toString), concat) else false)
     }
 
-  override def parse(list: List[String]): Map[BigInt, List[BigInt]] = list match {
-    case s"$total: $values" :: xs => parse(xs) + (BigInt(total) -> values.split(' ').withFilter(_.nonEmpty).map(BigInt(_)).toList)
-    case _ => Map.empty
-  }
+  override def parse(list: List[String]): List[(BigInt, List[BigInt])] =
+    list.map { case s"$total: $values" => BigInt(total) -> values.split(' ').withFilter(_.nonEmpty).map(BigInt(_)).toList }
 
-  override def solution1(input: Map[BigInt, List[BigInt]]): BigInt =
+  override def solution1(input: List[(BigInt, List[BigInt])]): BigInt =
     input.withFilter((total, values) => checkCalculation(values.tail, total, values.head)).map(_._1).sum
 
-  override def solution2(input: Map[BigInt, List[BigInt]]): BigInt =
+  override def solution2(input: List[(BigInt, List[BigInt])]): BigInt =
     input.withFilter((total, values) => checkCalculation(values.tail, total, values.head, true)).map(_._1).sum
 }
