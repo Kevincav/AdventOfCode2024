7b7a27f94e790f1b921e1681053b23c9d57d71a4
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-07 11:23:50 -0500

    s/BigInt/Long
    Signed-off-by: kevinmelkowski <kevin.melkowski@gmail.com>

--- src/main/scala/Solutions/Day7.scala
+++ src/main/scala/Solutions/Day7.scala
@@ -2,20 +2,20 @@ package Solutions
 
 import utils.Problem
 
-object Day7 extends Problem[List[(BigInt, List[BigInt])]](2024, 7) {
-  private def checkCalculation(values: List[BigInt], total: BigInt, result: BigInt, concat: Boolean = false): Boolean =
+object Day7 extends Problem[List[(Long, List[Long])]](2024, 7) {
+  private def checkCalculation(values: List[Long], total: Long, result: Long, concat: Boolean = false): Boolean =
     values match {
       case Nil => if (result == total) true else false
       case x :: xs => checkCalculation(xs, total, result + x, concat) || checkCalculation(xs, total, result * x, concat)
-        || (if (concat) checkCalculation(xs, total, BigInt(result.toString + x.toString), concat) else false)
+        || (if (concat) checkCalculation(xs, total, (result.toString + x.toString).toLong, concat) else false)
     }
 
-  override def parse(list: List[String]): List[(BigInt, List[BigInt])] =
-    list.map { case s"$total: $values" => BigInt(total) -> values.split(' ').map(BigInt(_)).toList }
+  override def parse(list: List[String]): List[(Long, List[Long])] =
+    list.map { case s"$total: $values" => total.toLong -> values.split(' ').map(_.toLong).toList }
 
-  override def solution1(input: List[(BigInt, List[BigInt])]): BigInt =
+  override def solution1(input: List[(Long, List[Long])]): Long =
     input.withFilter((total, values) => checkCalculation(values.tail, total, values.head)).map(_._1).sum
 
-  override def solution2(input: List[(BigInt, List[BigInt])]): BigInt =
+  override def solution2(input: List[(Long, List[Long])]): Long =
     input.withFilter((total, values) => checkCalculation(values.tail, total, values.head, true)).map(_._1).sum
 }
