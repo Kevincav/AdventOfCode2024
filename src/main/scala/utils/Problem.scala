52715ba9a16085e2fbcc80fad830f18e3aafff72
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-07 12:52:23 -0500

    Redundant if
    Signed-off-by: kevinmelkowski <kevin.melkowski@gmail.com>

--- src/main/scala/utils/Problem.scala
+++ src/main/scala/utils/Problem.scala
@@ -10,15 +10,14 @@ import java.nio.file.StandardOpenOption.{CREATE, WRITE}
 import scala.io.Source
 
 abstract class Problem[A](year: Int, day: Int) {
-  private def checkFolderExists(): Unit = if (Files.notExists(Paths.get(s"src/main/resources/$year/$day")))
-    Files.createDirectories(Paths.get(s"src/main/resources/$year/$day"))
-
   private def getDaysInput: String = quickRequest
     .get(uri"https://adventofcode.com/$year/day/$day/input")
     .cookie("session", Source.fromResource("token").getLines().toList.head)
     .header("User-Agent", "advent-of-code-data scala-v1").send(DefaultSyncBackend()).body
 
   private def getTodayData(testRun: Boolean): List[String] = {
+    if (Files.notExists(Paths.get(s"src/main/resources/$year/$day")))
+      Files.createDirectories(Paths.get(s"src/main/resources/$year/$day"))
     val path = Paths.get(s"src/main/resources/$year/$day/solution")
     if (Files.notExists(path)) Files.writeString(path, getDaysInput, CREATE, WRITE)
     Source.fromResource(s"$year/$day/${if (testRun) "example" else "solution"}").getLines().toList
@@ -31,7 +30,6 @@ abstract class Problem[A](year: Int, day: Int) {
   def solution2(input: A): Any
 
   def run(testRun: Boolean = false): Unit = {
-    checkFolderExists()
     val solutionInput = getTodayData(testRun)
     val solution = parse(solutionInput)
     println(Timer { solution1(solution) })
