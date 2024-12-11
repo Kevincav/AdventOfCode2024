2544ca5ff4777fb8fcb8c02157838a38bd6e77c2
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-11 01:06:44 -0500

    Added new template to create days source files
    Signed-off-by: kevinmelkowski <kevin.melkowski@gmail.com>

---
+++ src/main/scala/org/advent/year2024/Day11.scala
@@ -0,0 +1,11 @@
+package org.advent.year2024
+
+import org.advent.utils.Problem
+
+object Day11 extends Problem[Boolean](2024, 11) {
+  override def setup(input: List[String]): Boolean = true
+
+  override def solution1(data: Boolean): Int = 0
+
+  override def solution2(data: Boolean): Int = 0
+}
