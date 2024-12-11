8f35fccf1d6d607ba1607503ff32c4b39b044f3f
kevinmelkowski <kevin.melkowski@gmail.com>
2024-12-11 14:13:08 -0500

    Day 11 update
    Signed-off-by: kevinmelkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2024/Day11.scala
+++ src/main/scala/org/advent/year2024/Day11.scala
@@ -2,16 +2,19 @@ package org.advent.year2024
 
 import org.advent.utils.Problem
 
+import scala.collection.mutable
+
 object Day11 extends Problem[List[Long]](2024, 11) {
   extension (a: Long)
     private inline def length: Int = math.log10(math.abs(a)).toInt + 1
     private inline def split: (Long, Long) = { val mul = math.pow(10, a.length / 2); ((a / mul).toLong, (a % mul).toLong) }
 
-  private def blink(stone: Long, count: Int): Long =
-    if (count == 0) 1
-    else if (stone == 0) blink(1, count - 1)
-    else if (stone.length % 2 == 0) { val (left, right) = stone.split; blink(left, count - 1) + blink(right, count - 1) }
-    else blink(stone * 2024, count - 1)
+  private def blink(stone: Long, count: Int, map: mutable.Map[(Long, Long), Long] = mutable.Map.empty): Long =
+    if (count == 0) { map((stone, count)) = 1; 1 } else if (map.contains((stone, count))) map((stone, count)) else {
+      map((stone, count)) = if (stone == 0) blink(1, count - 1, map) else if (stone.length % 2 == 0) { val (left, right) =
+        stone.split; blink(left, count - 1, map) + blink(right, count - 1, map) } else blink(stone * 2024, count - 1, map)
+      map((stone, count))
+    }
 
   override def setup(input: List[String]): List[Long] = List.from(input.head.split(" ")).map(_.toLong)
 
