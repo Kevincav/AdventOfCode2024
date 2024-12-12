45efd9ee6e40009330cd3680093b1ce7b9b1694e
Kevincav <Kevincav@users.noreply.github.com>
2024-12-12 05:39:04 +0000

    Daily AoC Source Files

---
+++ src/main/scala/org/advent/year2024/Day12.scala
@@ -0,0 +1,11 @@
+package org.advent.year2024
+
+import org.advent.utils.Problem
+
+object Day12 extends Problem[Boolean](2024, 12) {
+  override def setup(input: List[String]): Boolean = true
+
+  override def solution1(data: Boolean): Int = 0
+
+  override def solution2(data: Boolean): Int = 0
+}
