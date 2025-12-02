70d40c973ec79e86c852ef7058e31b41d88bee31
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-01 23:52:30 -0700

    Day 2 solution
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2025/Day2.scala
+++ src/main/scala/org/advent/year2025/Day2.scala
@@ -2,10 +2,28 @@ package org.advent.year2025
 
 import org.advent.utils.Problem
 
-object Day2 extends Problem[Boolean](2025, 2) {
-  override def setup(input: List[String]): Boolean = true
+import scala.collection.immutable.NumericRange
 
-  override def solution1(data: Boolean): Long = 0
+object Day2 extends Problem[Array[NumericRange.Inclusive[Long]]](2025, 2) {
+  private def isValid(value: Long): Boolean = {
+    def countSubString(string: String, substring: String): Int =
+      string.sliding(substring.length, substring.length).count(_ == substring)
 
-  override def solution2(data: Boolean): Long = 0
+    val subStrings = value.toString.splitAt(value.toString.length / 2)._1.scanLeft("")(_ + _).tail
+    subStrings.collectFirst { case subString if countSubString(value.toString, subString) * subString.length == value.toString.length => subString }.isDefined
+  }
+
+  override def setup(input: List[String]): Array[NumericRange.Inclusive[Long]] =
+    input.head.split(",").map(range => { val split = range.split("-"); split.head.toLong to split.last.toLong })
+
+  override def solution1(data: Array[NumericRange.Inclusive[Long]]): Long = {
+    def isValid(value: BigInt): Boolean = if (value.toString.length % 2 == 1) false else {
+      val split = value.toString.splitAt(value.toString.length / 2)
+      split.head == split.last
+    }
+
+    data.map(_.filter(isValid).sum).sum
+  }
+
+  override def solution2(data: Array[NumericRange.Inclusive[Long]]): Long = data.map(_.filter(isValid).sum).sum
 }
--- src/test/scala/org/advent/year2025/Day2Test.scala
+++ src/test/scala/org/advent/year2025/Day2Test.scala
@@ -7,15 +7,15 @@ import cats.effect.unsafe.implicits.global
 
 class Day2Test extends AnyFunSuite with Matchers {
   test("Day 2 Part 1") {
-    val data = s"""|""".stripMargin.split("\n").toList
+    val data = s"""|11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124""".stripMargin.split("\n").toList
 
-    Day2.solution1(Day2.setup(data)) shouldBe 0
+    Day2.solution1(Day2.setup(data)) shouldBe 1227775554L
   }
 
   test("Day 2 Part 2") {
-    val data = s"""|""".stripMargin.split("\n").toList
+    val data = s"""|11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124""".stripMargin.split("\n").toList
 
-    Day2.solution2(Day2.setup(data)) shouldBe 0
+    Day2.solution2(Day2.setup(data)) shouldBe 4174379265L
   }
 
   test("Run Day 2") {
