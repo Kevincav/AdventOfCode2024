005300e662981cca9436103321f0e1d1fb8eb8fa
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-02 00:06:52 -0700

    Day 2 solution updated with cleaner solution
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2025/Day2.scala
+++ src/main/scala/org/advent/year2025/Day2.scala
@@ -5,25 +5,20 @@ import org.advent.utils.Problem
 import scala.collection.immutable.NumericRange
 
 object Day2 extends Problem[Array[NumericRange.Inclusive[Long]]](2025, 2) {
-  private def isValid(value: Long): Boolean = {
-    def countSubString(string: String, substring: String): Int =
-      string.sliding(substring.length, substring.length).count(_ == substring)
+  private def findDivisors(value: Long)(length: Int = value.toString.length): List[Int] =
+    (1 to length).filter(x => length % x == 0 && length / x >= 2).toList
 
-    val subStrings = value.toString.splitAt(value.toString.length / 2)._1.scanLeft("")(_ + _).tail
-    subStrings.collectFirst { case subString if countSubString(value.toString, subString) * subString.length == value.toString.length => subString }.isDefined
+  private def isValid(value: Long, size: Int): Boolean = {
+    val subStrings = value.toString.sliding(size, size).toList
+    subStrings.tail.forall(_ == subStrings.head)
   }
 
   override def setup(input: List[String]): Array[NumericRange.Inclusive[Long]] =
     input.head.split(",").map(range => { val split = range.split("-"); split.head.toLong to split.last.toLong })
 
-  override def solution1(data: Array[NumericRange.Inclusive[Long]]): Long = {
-    def isValid(value: BigInt): Boolean = if (value.toString.length % 2 == 1) false else {
-      val split = value.toString.splitAt(value.toString.length / 2)
-      split.head == split.last
-    }
+  override def solution1(data: Array[NumericRange.Inclusive[Long]]): Long =
+    data.map(_.filter(value => value.toString.length % 2 == 0 && isValid(value, value.toString.length / 2)).sum).sum
 
-    data.map(_.filter(isValid).sum).sum
-  }
-
-  override def solution2(data: Array[NumericRange.Inclusive[Long]]): Long = data.map(_.filter(isValid).sum).sum
+  override def solution2(data: Array[NumericRange.Inclusive[Long]]): Long =
+    data.map(_.filter(value => findDivisors(value)().exists(i => isValid(value, i))).sum).sum
 }
