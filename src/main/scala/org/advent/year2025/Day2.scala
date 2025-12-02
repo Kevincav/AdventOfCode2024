3cdbaf4afcd6dbe3756c14773d7702255a819fb7
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-02 00:33:55 -0700

    Day 2 solution, optimized with lazy lists
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2025/Day2.scala
+++ src/main/scala/org/advent/year2025/Day2.scala
@@ -5,20 +5,21 @@ import org.advent.utils.Problem
 import scala.collection.immutable.NumericRange
 
 object Day2 extends Problem[Array[NumericRange.Inclusive[Long]]](2025, 2) {
-  private def findDivisors(value: Long)(length: Int = value.toString.length): List[Int] =
-    (1 to length).filter(x => length % x == 0 && length / x >= 2).toList
+  private def findDivisors(len: Int): List[Int] = (1 to len).filter(d => len % d == 0 && len / d >= 2).toList
 
-  private def isValid(value: Long, size: Int): Boolean = {
-    val subStrings = value.toString.sliding(size, size).toList
-    subStrings.tail.forall(_ == subStrings.head)
+  private def isValidDigits(digits: String, size: Int): Boolean = {
+    val chunk = digits.take(size)
+    digits.grouped(size).forall(_ == chunk)
   }
 
-  override def setup(input: List[String]): Array[NumericRange.Inclusive[Long]] =
-    input.head.split(",").map(range => { val split = range.split("-"); split.head.toLong to split.last.toLong })
+  override def setup(input: List[String]): Array[NumericRange.Inclusive[Long]] = input.head.split(",").map { range =>
+    val Array(low, high) = range.split("-")
+    low.toLong to high.toLong
+  }
 
   override def solution1(data: Array[NumericRange.Inclusive[Long]]): Long =
-    data.map(_.filter(value => value.toString.length % 2 == 0 && isValid(value, value.toString.length / 2)).sum).sum
+    data.map(_.filter { value => value.toString.length % 2 == 0 && isValidDigits(value.toString, value.toString.length / 2) }.sum).sum
 
   override def solution2(data: Array[NumericRange.Inclusive[Long]]): Long =
-    data.map(_.filter(value => findDivisors(value)().exists(i => isValid(value, i))).sum).sum
+    data.map(_.filter { value => findDivisors(value.toString.length).exists(size => isValidDigits(value.toString, size)) }.sum).sum
 }
