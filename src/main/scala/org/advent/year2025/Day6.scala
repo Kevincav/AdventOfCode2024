7befa9945aec5258c3244683bb47a235c189a649
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-05 23:58:42 -0700

    Day 6 solution
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2025/Day6.scala
+++ src/main/scala/org/advent/year2025/Day6.scala
@@ -2,10 +2,25 @@ package org.advent.year2025
 
 import org.advent.utils.Problem
 
-object Day6 extends Problem[Boolean](2025, 6) {
-  override def setup(input: List[String]): Boolean = true
+object Day6 extends Problem[(List[List[String]], List[Char])](2025, 6) {
+  private def parseColumn(list: List[String]): List[Long] = list.head.indices.reverse.map(j =>
+    list.indices.foldLeft(0L)((sum, i) => if (list(i)(j) == ' ') sum else sum * 10 + list(i)(j).asDigit)).toList
 
-  override def solution1(data: Boolean): Long = 0
+  private def calculate(list: List[List[Long]], operators: List[Char]) =
+    operators.indices.map(i => if (operators(i) == '+') list(i).sum else list(i).product).sum
 
-  override def solution2(data: Boolean): Long = 0
+  override def setup(input: List[String]): (List[List[String]], List[Char]) = {
+    def getSubstringIndexes(input: List[String]): List[(Int, Int)] = {
+      val subIndexes = input.map(_.toCharArray).map(_.zipWithIndex.withFilter(_._1 == ' ').map(_._2))
+      val indexes = (0 :: subIndexes.reduce { case (x, y) => x.intersect(y) }.flatMap(i => List(i, i + 1)).sorted.toList) :+ input.map(_.length).max
+      indexes.sliding(2, 2).map(index => (index.head, index.last)).toList
+    }
+
+    (input.dropRight(1).map(row => getSubstringIndexes(input.dropRight(1)).map((i, j) => row.substring(i, j))).transpose,
+      input.last.split(' ').withFilter(_.nonEmpty).map(_.head).toList)
+  }
+
+  override def solution1(data: (List[List[String]], List[Char])): Long = calculate(data._1.map(_.map(_.strip().toLong)), data._2)
+
+  override def solution2(data: (List[List[String]], List[Char])): Long = calculate(data._1.map(parseColumn), data._2)
 }
--- src/test/scala/org/advent/year2025/Day6Test.scala
+++ src/test/scala/org/advent/year2025/Day6Test.scala
@@ -7,15 +7,15 @@ import cats.effect.unsafe.implicits.global
 
 class Day6Test extends AnyFunSuite with Matchers {
   test("Day 6 Part 1") {
-    val data = s"""|""".stripMargin.split("\n").toList
+    val data = s"""123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  """.split('\n').toList
 
-    Day6.solution1(Day6.setup(data)) shouldBe 0
+    Day6.solution1(Day6.setup(data)) shouldBe 4277556
   }
 
   test("Day 6 Part 2") {
-    val data = s"""|""".stripMargin.split("\n").toList
-
-    Day6.solution2(Day6.setup(data)) shouldBe 0
+    val data = s"""123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  """.split('\n').toList
+    
+    Day6.solution2(Day6.setup(data)) shouldBe 3263827
   }
 
   test("Run Day 6") {
