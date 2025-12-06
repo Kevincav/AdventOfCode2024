65be9650258907e37ec9a5fdb0a4a506718843bc
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-06 00:30:59 -0700

    Day 6 cleanup
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2025/Day6.scala
+++ src/main/scala/org/advent/year2025/Day6.scala
@@ -11,7 +11,7 @@ object Day6 extends Problem[(List[List[String]], List[Char])](2025, 6) {
 
   override def setup(input: List[String]): (List[List[String]], List[Char]) = {
     def getSubstringRanges(input: List[String]): List[(Int, Int)] = {
-      val subIndexes = input.map(_.toCharArray.zipWithIndex.withFilter(_._1 == ' ').map(_._2))
+      val subIndexes = input.map(_.zipWithIndex.withFilter(_._1 == ' ').map(_._2))
       val indexes = (0 +: subIndexes.reduce(_ intersect _).flatMap(i => List(i, i + 1)).sorted) :+ input.map(_.length).max
       indexes.sliding(2, 2).map(index => (index.head, index.last)).toList
     }
@@ -19,7 +19,7 @@ object Day6 extends Problem[(List[List[String]], List[Char])](2025, 6) {
     def getSubStrings(rows: List[String]): List[List[String]] =
       rows.map(row => getSubstringRanges(rows).map((i, j) => row.substring(i, j))).transpose
 
-    (getSubStrings(input.dropRight(1)), input.last.split(' ').withFilter(_.nonEmpty).map(_.head).toList)
+    (getSubStrings(input.dropRight(1)), input.last.filter(_ != ' ').toList)
   }
 
   override def solution1(data: (List[List[String]], List[Char])): Long = calculate(data._1.map(_.map(_.strip().toLong)), data._2)
