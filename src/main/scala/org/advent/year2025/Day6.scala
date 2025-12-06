0ddb350461f02ad8ca62e19e8c286569c02de595
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-06 00:05:58 -0700

    Day 6 cleanup
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- src/main/scala/org/advent/year2025/Day6.scala
+++ src/main/scala/org/advent/year2025/Day6.scala
@@ -12,12 +12,15 @@ object Day6 extends Problem[(List[List[String]], List[Char])](2025, 6) {
   override def setup(input: List[String]): (List[List[String]], List[Char]) = {
     def getSubstringIndexes(input: List[String]): List[(Int, Int)] = {
       val subIndexes = input.map(_.toCharArray).map(_.zipWithIndex.withFilter(_._1 == ' ').map(_._2))
-      val indexes = (0 :: subIndexes.reduce { case (x, y) => x.intersect(y) }.flatMap(i => List(i, i + 1)).sorted.toList) :+ input.map(_.length).max
+      val indexes = (0 :: subIndexes.reduce { case (x, y) => x.intersect(y) }
+        .flatMap(i => List(i, i + 1)).sorted.toList) :+ input.map(_.length).max
       indexes.sliding(2, 2).map(index => (index.head, index.last)).toList
     }
 
-    (input.dropRight(1).map(row => getSubstringIndexes(input.dropRight(1)).map((i, j) => row.substring(i, j))).transpose,
-      input.last.split(' ').withFilter(_.nonEmpty).map(_.head).toList)
+    def getSubStrings(rows: List[String]): List[List[String]] =
+      rows.map(row => getSubstringIndexes(rows).map((i, j) => row.substring(i, j))).transpose
+
+    (getSubStrings(input.dropRight(1)), input.last.split(' ').withFilter(_.nonEmpty).map(_.head).toList)
   }
 
   override def solution1(data: (List[List[String]], List[Char])): Long = calculate(data._1.map(_.map(_.strip().toLong)), data._2)
