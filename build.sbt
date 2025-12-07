4f342bf8becbc4a18d10988ec3e68550c3e41c8d
Kevin Melkowski <kevin.melkowski@gmail.com>
2025-12-06 21:05:15 -0700

    Adding cats range for longs
    Signed-off-by: Kevin Melkowski <kevin.melkowski@gmail.com>

--- build.sbt
+++ build.sbt
@@ -7,9 +7,10 @@ lazy val root = project
     libraryDependencies ++= Seq(
       "org.scalactic" %% "scalactic" % "3.2.19",
       "org.scalatest" %% "scalatest" % "3.2.19" % Test,
-      "org.typelevel" %% "cats-effect" % "3.6.1",
+      "org.typelevel" %% "cats-effect" % "3.6.3",
+      "org.typelevel" %% "cats-collections-core" % "0.9.10",
       "com.github.pathikrit" %% "better-files" % "3.9.2",
-      "com.typesafe.play" %% "play-json" % "2.10.6",
+      "com.typesafe.play" %% "play-json" % "2.10.8",
       "org.scala-lang.modules" %% "scala-parallel-collections" % "1.2.0",
       "io.github.kevincav" %% "rate-limiter" % "v1.0.2",
     ))
--- src/main/scala/org/advent/year2025/Day5.scala
+++ src/main/scala/org/advent/year2025/Day5.scala
@@ -2,36 +2,26 @@ package org.advent.year2025
 
 import org.advent.utils.Problem
 
+import cats.collections.Range
 import scala.annotation.tailrec
 
-object Day5 extends Problem[(List[(Long, Long)], List[Long])](2025, 5) {
-  override def setup(input: List[String]): (List[(Long, Long)], List[Long]) = {
+object Day5 extends Problem[(List[Range[Long]], List[Long])](2025, 5) {
+  override def setup(input: List[String]): (List[Range[Long]], List[Long]) = {
     @tailrec
-    def mergeRanges(list: List[(Long, Long)], result: List[(Long, Long)] = List.empty): List[(Long, Long)] =
+    def mergeRanges(list: List[Range[Long]], result: List[Range[Long]] = List.empty): List[Range[Long]] =
       list match {
         case Nil => result.reverse
         case x :: Nil => (x :: result).reverse
-        case x :: y :: xs if x._2 >= y._1 => mergeRanges((x._1, math.max(x._2, y._2)) :: xs, result)
+        case Range(x1, x2) :: Range(y1, y2) :: xs if x2 >= y1 => mergeRanges(Range(x1, math.max(x2, y2)) :: xs, result)
         case x :: y :: xs => mergeRanges(y :: xs, x :: result)
       }
 
-    val range = """(\d+)-(\d+)""".r
-    val idx = """(\d+)""".r
-
-    val (ranges, ids) = input.foldLeft((List.empty[(Long, Long)], List.empty[Long])) {
-      case ((ranges, ids), row) => row match {
-        case range(start, stop) => ((start.toLong, stop.toLong) :: ranges, ids)
-        case idx(id) => (ranges, id.toLong :: ids)
-        case _ => (ranges, ids)
-      }
-    }
-
-    (mergeRanges(ranges.sortBy(_._1)), ids)
+    val (ranges, ids) = input.splitAt(input.zipWithIndex.find(_._1.isEmpty).get._2)
+    (mergeRanges(ranges.map { case s"$start-$end" => Range(start.toLong, end.toLong) }.sortBy(_._1)), ids.drop(1).map(_.toLong))
   }
 
-  override def solution1(data: (List[(Long, Long)], List[Long])): Long =
-    data._2.count(id => data._1.exists((from, to) => id >= from && id <= to))
+  override def solution1(data: (List[Range[Long]], List[Long])): Long = data._2.count(id => data._1.exists(_.contains(id)))
 
-  override def solution2(data: (List[(Long, Long)], List[Long])): Long =
-    data._1.foldLeft(0L) { case (sum, (from, to)) => sum + (to - from) + 1 }
+  override def solution2(data: (List[Range[Long]], List[Long])): Long =
+    data._1.foldLeft(0L) { case (sum, range) => sum + (range.end - range.start) + 1 }
 }
