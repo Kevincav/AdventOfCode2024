c4cb0d64472eddb97d63d7c5d04ad72a51efb624
Kevincav <Kevincav@users.noreply.github.com>
2025-12-12 05:13:08 +0000

    Daily input file update

---
+++ src/test/scala/org/advent/year2025/Day12Test.scala
@@ -0,0 +1,24 @@
+package org.advent.year2025
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import cats.effect.unsafe.implicits.global
+
+class Day12Test extends AnyFunSuite with Matchers {
+  test("Day 12 Part 1") {
+    val data = s"""|""".stripMargin.split("\n").toList
+
+    Day12.solution1(Day12.setup(data)) shouldBe 0
+  }
+
+  test("Day 12 Part 2") {
+    val data = s"""|""".stripMargin.split("\n").toList
+
+    Day12.solution2(Day12.setup(data)) shouldBe 0
+  }
+
+  test("Run Day 12") {
+    Day12.run().unsafeRunSync()
+  }
+}
